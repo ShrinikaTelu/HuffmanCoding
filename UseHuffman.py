3e7e5501c9ed6c894e95812bee356c04213ad607
SHRINIKA TELU <65474534+ShrinikaTelu@users.noreply.github.com>
2021-08-11 04:14:12 +0530

    first commit

--- UseHuffman.py
+++ UseHuffman.py
@@ -8,15 +8,10 @@ from huffman import HuffmanCoding
 import sys
 
 path = "/Users/shrinika/Documents/HuffmanCoding_Project/sample.txt"
-
 h = HuffmanCoding(path)
-
-print('Compressing file , please wait !')
 
 com_path = h.compress()
 print("Compressed file path: " + com_path)
-
-print('Decompressing file , please wait !')
 
 decom_path = h.decompress(com_path)
 print("Decompressed file path: " + decom_path)
